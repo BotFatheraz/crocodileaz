ee3a0f884912ba5e39e0f3e68051b72db122e4ba
BotFatheraz <76791824+BotFatheraz@users.noreply.github.com>
2021-01-19 17:52:45 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+PROXY_URL = 'https://54.37.131.45:3128'
+REQUEST_KWARGS = {'proxy_url': 'https://54.37.131.45:3128'}
+TOKEN = "1538550890:AAFRJikeOHPj7hZOt5KeA6jhUc5eP22dIMc"
+with open('words.txt', 'r', encoding='utf-8') as file:
+    word_list = file.read().splitlines()
